8ca6adf13be767131fbd439c46f6221cf4285946
BakayAlexander <bakay.dvr@gmail.com>
2022-05-02 10:41:25 +0300

    feat: finish styling Login and AuthForm

--- src/Login/Login.js
+++ src/Login/Login.js
@@ -4,7 +4,8 @@ import './Login.css';
 
 function Login() {
 	return (
-		<section>
+		<section className='login'>
+			<h2 className='login__title'>Welcome! Please sign in.</h2>
 			<AuthForm buttonName='Login' linkPath='/signup' linkName='Register' />
 		</section>
 	);
